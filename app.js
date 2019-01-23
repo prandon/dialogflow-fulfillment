56ea16aa8213b81da82daabe162aa5cf763bdad8
Pranjal Nartam <Pranjal.Nartam@capco.com>
2019-01-23 15:06:50 +0530

    port number changed

--- app.js
+++ app.js
@@ -20,4 +20,4 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 app.use("/product", product)
 
-app.listen(5000, () => console.log('Server Started at 5000'))
+app.listen(process.env.PORT || 5000, () => console.log('Server Started at 5000'))
